f9230c2df81c1b011532ead43ea0751f7f6b99bb
nikolai krylov <n.krylov@solarsecurity.ru>
2018-03-28 18:33:03 +0300

    minimum viable product

---
+++ src/main/java/com/bmorge/Case.java
@@ -0,0 +1,59 @@
+package com.bmorge;
+
+class Case {
+    private final Object sample;
+    private boolean isEquals;
+    private boolean fulfilledBefore;
+
+    private Case() {
+        this.sample = null;
+    }
+
+    Case(Object sample) {
+        this.sample = sample;
+    }
+
+    public Case caser(Object caser) {
+        if (fulfilledBefore) return this;
+        mainEquals(caser);
+        return this;
+    }
+
+    public Case caser(Object... caser) {
+        if (fulfilledBefore) return this;
+        for (Object example : caser) {
+            mainEquals(example);
+            if (isEquals) break;
+        }
+        return this;
+    }
+
+    private void mainEquals(Object example) {
+        if (sample == example) {
+            isEquals = true;
+            return;
+        }
+        if (sample == null) {
+            return;
+        }
+        if (sample.equals(example)) {
+            isEquals = true;
+        }
+    }
+
+    public Case breaker(Runnable runnable) {
+        if (isEquals) {
+            runnable.run();
+            fulfilledBefore = true;
+        }
+        isEquals = false;
+        return this;
+    }
+
+    public void onDefault(Runnable runnable) {
+        if (!fulfilledBefore) {
+            runnable.run();
+        }
+    }
+
+}
--- src/main/java/com/bmorge/SwitchCaser.java
+++ src/main/java/com/bmorge/SwitchCaser.java
@@ -1,4 +1,17 @@
 package com.bmorge;
 
 public class SwitchCaser {
+
+    private SwitchCaser(){
+    }
+
+    public static Case switchIt(Object sample){
+        return new Case(sample);
+    }
+
+    public static <T extends Cloneable> Case switchItClone(T sample){
+        //todo Clone it
+        return new Case(sample);
+    }
+
 }
